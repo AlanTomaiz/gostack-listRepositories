75234872e7819f10c074737e0b3e2dd2745df1d8
Alanderson Tomaiz <alan_tomaiz@hotmail.com>
2020-11-30 11:33:40 -0300

    Styled dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,64 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoSVG from '../../assets/logo.svg';
+import { Title, Form, Repositories } from './styles';
 
-const Dashboard: React.FC = () => <Title>Explore repositórios no GitHub</Title>;
+const Dashboard: React.FC = () => (
+  <>
+    <img src={logoSVG} alt="GitHub" />
+    <Title>Explore repositórios no GitHub</Title>
+    <Form>
+      <input placeholder="Digite o nome do repositório" />
+      <button type="submit">Pesquisar</button>
+    </Form>
+    <Repositories>
+      <a href="teste">
+        <img
+          src="https://avatars1.githubusercontent.com/u/26505634?s=460&u=ae8857317b5c01c3e7cd0e995bb3c3cd9513abf0&v=4"
+          alt="Alanderson Tomaiz"
+        />
+        <div>
+          <strong>AlanTomaiz/gostack-listRepositories</strong>
+          <p>Getting Started learning ReactJS with Typescript</p>
+        </div>
+        <FiChevronRight />
+      </a>
+      <a href="teste">
+        <img
+          src="https://avatars1.githubusercontent.com/u/26505634?s=460&u=ae8857317b5c01c3e7cd0e995bb3c3cd9513abf0&v=4"
+          alt="Alanderson Tomaiz"
+        />
+        <div>
+          <strong>AlanTomaiz/gostack-listRepositories</strong>
+          <p>Getting Started learning ReactJS with Typescript</p>
+        </div>
+        <FiChevronRight />
+      </a>
+      <a href="teste">
+        <img
+          src="https://avatars1.githubusercontent.com/u/26505634?s=460&u=ae8857317b5c01c3e7cd0e995bb3c3cd9513abf0&v=4"
+          alt="Alanderson Tomaiz"
+        />
+        <div>
+          <strong>AlanTomaiz/gostack-listRepositories</strong>
+          <p>Getting Started learning ReactJS with Typescript</p>
+        </div>
+        <FiChevronRight />
+      </a>
+      <a href="teste">
+        <img
+          src="https://avatars1.githubusercontent.com/u/26505634?s=460&u=ae8857317b5c01c3e7cd0e995bb3c3cd9513abf0&v=4"
+          alt="Alanderson Tomaiz"
+        />
+        <div>
+          <strong>AlanTomaiz/gostack-listRepositories</strong>
+          <p>Getting Started learning ReactJS with Typescript</p>
+        </div>
+        <FiChevronRight />
+      </a>
+    </Repositories>
+  </>
+);
 
 export default Dashboard;
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,6 +1,93 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
 export const Title = styled.h1`
   font-size: 48px;
   color: #3a3a3a;
+  max-width: 450px;
+  line-height: 56px;
+  margin-top: 80px;
+`;
+
+export const Form = styled.form`
+  margin-top: 40px;
+  max-width: 700px;
+  display: flex;
+
+  input {
+    flex: 1;
+    height: 70px;
+    color: #3a3a3a;
+    padding: 0 24px;
+    border: 0;
+    border-radius: 5px 0 0 5px;
+
+    &::placeholder {
+      color: #a8a8b3;
+    }
+  }
+
+  button {
+    width: 210px;
+    height: 70px;
+    background: #04d361;
+    color: #fff;
+    font-weight: bold;
+    border: 0;
+    border-radius: 0 5px 5px 0;
+    transition: 0.2s;
+
+    &:hover {
+      background: ${shade(0.2, '#04d361')};
+    }
+  }
+`;
+
+export const Repositories = styled.div`
+  margin-top: 80px;
+  max-width: 700px;
+
+  a {
+    display: flex;
+    align-items: center;
+    background: #fff;
+    border-radius: 5px;
+    padding: 24px;
+    text-decoration: none;
+    transition: 0.2s;
+
+    &:hover {
+      transform: translateX(10px);
+    }
+
+    & + a {
+      margin-top: 16px;
+    }
+
+    img {
+      width: 64px;
+      height: 64px;
+      border-radius: 50%;
+    }
+
+    div {
+      margin-left: 16px;
+
+      strong {
+        font-size: 20px;
+        color: #3d3d4b;
+      }
+
+      p {
+        font-size: 18px;
+        color: #a8a8b3;
+        margin-top: 4px;
+      }
+    }
+
+    svg {
+      margin-left: auto;
+      color: #3d3d4b;
+    }
+  }
 `;
