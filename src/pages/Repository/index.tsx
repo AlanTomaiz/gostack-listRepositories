d7cd5ab65f21dd1190c896d5d88a5217288ed38d
Alanderson Tomaiz <alan_tomaiz@hotmail.com>
2020-11-30 13:49:48 -0300

    Update router repository

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
-const Repository: React.FC = () => <h1>Hello GitHub</h1>;
+interface RepoParams {
+  repository: string;
+}
+
+const Repository: React.FC = () => {
+  const { repository } = useParams<RepoParams>();
+
+  return <h1>Hello GitHub:{repository}</h1>;
+};
 
 export default Repository;
