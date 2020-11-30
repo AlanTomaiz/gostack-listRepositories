64ab5e2242164cd33ca21d55549a6c86ace46d72
Alanderson Tomaiz <alan_tomaiz@hotmail.com>
2020-11-30 12:01:13 -0300

    Consuming API from GitHub and showing in dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,64 +1,62 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 import logoSVG from '../../assets/logo.svg';
 import { Title, Form, Repositories } from './styles';
 
-const Dashboard: React.FC = () => (
-  <>
-    <img src={logoSVG} alt="GitHub" />
-    <Title>Explore repositórios no GitHub</Title>
-    <Form>
-      <input placeholder="Digite o nome do repositório" />
-      <button type="submit">Pesquisar</button>
-    </Form>
-    <Repositories>
-      <a href="teste">
-        <img
-          src="https://avatars1.githubusercontent.com/u/26505634?s=460&u=ae8857317b5c01c3e7cd0e995bb3c3cd9513abf0&v=4"
-          alt="Alanderson Tomaiz"
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
+const Dashboard: React.FC = () => {
+  const [repository, setRepository] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleGetRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    const { data: dataRepository } = await api.get<Repository>(
+      `/repos/${repository}`,
+    );
+
+    setRepositories([...repositories, dataRepository]);
+    setRepository('');
+  }
+
+  return (
+    <>
+      <img src={logoSVG} alt="GitHub" />
+      <Title>Explore repositórios no GitHub</Title>
+      <Form onSubmit={handleGetRepository}>
+        <input
+          value={repository}
+          onChange={(e) => setRepository(e.target.value)}
+          placeholder="Digite o nome do repositório"
         />
-        <div>
-          <strong>AlanTomaiz/gostack-listRepositories</strong>
-          <p>Getting Started learning ReactJS with Typescript</p>
-        </div>
-        <FiChevronRight />
-      </a>
-      <a href="teste">
-        <img
-          src="https://avatars1.githubusercontent.com/u/26505634?s=460&u=ae8857317b5c01c3e7cd0e995bb3c3cd9513abf0&v=4"
-          alt="Alanderson Tomaiz"
-        />
-        <div>
-          <strong>AlanTomaiz/gostack-listRepositories</strong>
-          <p>Getting Started learning ReactJS with Typescript</p>
-        </div>
-        <FiChevronRight />
-      </a>
-      <a href="teste">
-        <img
-          src="https://avatars1.githubusercontent.com/u/26505634?s=460&u=ae8857317b5c01c3e7cd0e995bb3c3cd9513abf0&v=4"
-          alt="Alanderson Tomaiz"
-        />
-        <div>
-          <strong>AlanTomaiz/gostack-listRepositories</strong>
-          <p>Getting Started learning ReactJS with Typescript</p>
-        </div>
-        <FiChevronRight />
-      </a>
-      <a href="teste">
-        <img
-          src="https://avatars1.githubusercontent.com/u/26505634?s=460&u=ae8857317b5c01c3e7cd0e995bb3c3cd9513abf0&v=4"
-          alt="Alanderson Tomaiz"
-        />
-        <div>
-          <strong>AlanTomaiz/gostack-listRepositories</strong>
-          <p>Getting Started learning ReactJS with Typescript</p>
-        </div>
-        <FiChevronRight />
-      </a>
-    </Repositories>
-  </>
-);
+        <button type="submit">Pesquisar</button>
+      </Form>
+      <Repositories>
+        {repositories.map((repo) => (
+          <a key={repo.full_name} href="teste">
+            <img src={repo.owner.avatar_url} alt={repo.owner.login} />
+            <div>
+              <strong>{repo.full_name}</strong>
+              <p>{repo.description}</p>
+            </div>
+            <FiChevronRight />
+          </a>
+        ))}
+      </Repositories>
+    </>
+  );
+};
 
 export default Dashboard;
